355f0ad20f03cf17c34cf3ad7091a5cdd6ea26cc
yoyo <g1831116@m.sojo-u.ac.jp>
2022-06-02 00:29:55 +0900

    sqlファイル追加

---
+++ demo/src/main/resources/data.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS employee;
+
+CREATE TABLE employee
+(
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(10),
+    company_name VARCHAR(20),
+    age INTEGER,
+    )
---
+++ demo/src/main/resources/schema.sql
@@ -0,0 +1,6 @@
+INSERT INTO employee VALUES(100, '田中', 'apple', 26);
+INSERT INTO employee VALUES(101, '中田', 'google', 58);
+INSERT INTO employee VALUES(102, '山田', 'facebook', 45);
+INSERT INTO employee VALUES(103, '所', 'amazon', 35);
+INSERT INTO employee VALUES(104, '松山', 'rakuten', 23);
+INSERT INTO employee VALUES(105, '小暮', 'kddi', 4507);
