533aa23dc7b3cb88f4ed2421025e7aa433b4d1d8
GM-Go <goto.goto.ma@gmail.com>
2022-06-08 01:20:35 +0900

    コントローラークラスの作成

--- demo/src/main/java/com/example/demo/web/response/EmployeeResponse.java
+++ demo/src/main/java/com/example/demo/web/response/EmployeeResponse.java
@@ -1,4 +1,49 @@
 package com.example.demo.web.response;
 
 public class EmployeeResponse {
+    // あえてpriveteで記述
+    private Integer id;
+    private String name;
+    private String companyName;
+    private Integer age;
+
+    // コンストラクタ
+    public EmployeeResponse(Integer id, String name, String companyName, Integer age) {
+        this.id = id;
+        this.name = name;
+        this.companyName = companyName;
+        this.age = age;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
 }
--- demo/src/main/java/com/example/demo/web/rest/EmployeeController.java
+++ demo/src/main/java/com/example/demo/web/rest/EmployeeController.java
@@ -1,4 +1,47 @@
 package com.example.demo.web.rest;
 
+import com.example.demo.persistence.entity.Employee;
+import com.example.demo.service.EmployeeService;
+import com.example.demo.web.response.EmployeeResponse;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/employee")
 public class EmployeeController {
+    // コンポーネントする
+    private final EmployeeService employeeService;
+
+    // コンストラクタ
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping
+    public List<EmployeeResponse> findAll() {
+        List<Employee> employeeList = employeeService.findAll();
+
+        // 型の変換
+        // 全件のデータをリストへ格納
+        List<EmployeeResponse> employeeResponseList = new ArrayList<>();
+        for (Employee employee : employeeList) {
+            EmployeeResponse employeeResponse = new EmployeeResponse(
+                    employee.getId(),
+                    employee.getName(),
+                    employee.getCompanyName(),
+                    employee.getAge());
+
+            employeeResponseList.add(employeeResponse);
+        }
+        // レスポンスリストを戻り値
+        return employeeResponseList;
+    }
+
+
+
 }
