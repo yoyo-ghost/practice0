60d58edefc62dcb1ab0c0ee39e349d5928c415c4
GM-Go <goto.goto.ma@gmail.com>
2022-06-07 22:08:11 +0900

    エンティティクラスの作成

--- demo/src/main/java/com/example/demo/persistence/entity/Employee.java
+++ demo/src/main/java/com/example/demo/persistence/entity/Employee.java
@@ -1,4 +1,40 @@
 package com.example.demo.persistence.entity;
 
 public class Employee {
+    private Integer id;
+    private String name;
+    private String companyName;
+    private Integer age;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
 }
--- demo/src/main/java/com/example/demo/persistence/mapper/EmployeeMapper.java
+++ demo/src/main/java/com/example/demo/persistence/mapper/EmployeeMapper.java
@@ -1,4 +1,5 @@
 package com.example.demo.persistence.mapper;
 
+
 public interface EmployeeMapper {
 }
